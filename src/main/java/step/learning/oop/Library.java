294a034f25afd5366542981fa89ae087948820f6
Lebe_fn09 <Lebe_fn09@student.itstep.org>
2023-07-13 13:44:43 +0300

    hw oop1 upd

--- src/main/java/step/learning/oop/Library.java
+++ src/main/java/step/learning/oop/Library.java
@@ -11,11 +11,24 @@ public class Library {
         funds.add(new Book("Art of Computer Programming", "Donald Knuth"));
         funds.add(new Book("The C Programming Language", "Dennis Ritchie"));
         funds.add(new Book("The C++ Programming Language", "Bjarne Stroustrup"));
+        funds.add(new Booklet("Java", "Oracle"));
+        funds.add(new Journal("Java Magazine", 1));
+        funds.add(new Newspaper("The New York Times", new java.util.Date()));
     }
 
     public void showCatalog() {
         for (Literature fund : funds) {
+            System.out.println();
             System.out.println(fund.getTitle());
+            if(fund instanceof Book) {
+                System.out.println("Author: " + ((Book) fund).getAuthor());
+            } else if (fund instanceof Booklet) {
+                System.out.println("Publisher: " + ((Booklet) fund).getPublisher());
+            } else if (fund instanceof Journal) {
+                System.out.println("Number: " + ((Journal) fund).getNumber());
+            } else if (fund instanceof Newspaper) {
+                System.out.println("Date: " + ((Newspaper) fund).getDate());
+            }
         }
     }
 }
